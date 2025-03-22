d01f66c1a49617dbce8fbb8345c8fb160c1236cd
Shubham Kangune <shubhamcsc4656@gmail.com>
2025-03-22 15:50:19 +0530

    add or remove the params

--- main.js
+++ main.js
@@ -1,6 +1,20 @@
 console.log("this is project 6 in js");
 
 
+// Utility functions:
+// 1. Utility function to get DOM element from string
+
+function getElementFromString(string) {
+    let div = document.createElement('div');
+    div.innerHTML = string;
+    return div.firstElementChild;
+}
+
+
+// initialize no of parameters
+let addedParamCount = 0;
+
+
 // hide the parameters box initially
 let parametersBox = document.getElementById('parametersBox');
 // parametersBox.style.display = 'none';
@@ -11,7 +25,7 @@ let parametersBox = document.getElementById('parametersBox');
 let paramesRadio = document.getElementById('paramesRadio')
 paramesRadio.addEventListener('click',()=>{
     document.getElementById('requestJsonBox').style.display = 'none'
-    document.getElementById('parametersBox').style.display = 'block'
+    document.getElementById('parametersBox').style.display = 'blockl'
 })
 
 // if the user clicks the json box, hide the params box
@@ -22,3 +36,57 @@ jsonRadio.addEventListener('click',()=>{
     document.getElementById('requestJsonBox').style.display = 'block'
 
 })
+
+
+let addParam = document.getElementById('addParam');
+addParam.addEventListener('click',()=>{
+    console.log("user click the addParam button");
+    let params = document.getElementById('params')  
+    let string = `<div id="parametersBox" class="flex gap-[15px] m-auto w-[570px] p-2">
+                        <label for="url" class="block mb-2 text-sm font-bold p-2 text-gray-900"
+                        >Parameter ${addedParamCount+2}</label
+                        >
+                        <div class="mb-5 col-md-4">
+                        <input
+                            type="text"
+                            id="parameterKey${addedParamCount+2}"
+                            class="bg-green-50 border border-green-500 text-green-900 dark:text-green-400 text-sm rounded-lg focus:ring-green-500 focus:border-green-500 block w-full p-2.5 dark:bg-gray-700 dark:border-green-500"
+                            placeholder="Enter Parameter ${addedParamCount+2} key"
+                        />
+                        </div>
+                        <div>
+                        <input
+                            type="text"
+                            id="parameterValue${addedParamCount+2}"
+                            class="border text-sm rounded-lg dark:bg-gray-700 block w-full p-2.5 dark:border-red-500"
+                            placeholder="Enter Parameter ${addedParamCount+2} value"
+                        />
+                        </div>
+
+                        <button
+                        type="button"
+                        id="addParam"
+                        class="btn deleteParam text-gray-900 bg-gradient-to-r from-red-200 via-red-300 to-yellow-200 hover:bg-gradient-to-bl focus:ring-4 focus:outline-none focus:ring-red-100 font-medium rounded-lg px-4 text-center h-10"
+                        >
+                        -
+                        </button>
+                    </div>`;
+
+    // convert the element string to DOM node
+    let paramElement = getElementFromString(string);
+    // console.log(paramElement);
+    params.appendChild(paramElement);
+    // add event listener to remove the parameter on clicking - button
+    let deleteParam = document.getElementsByClassName('deleteParam');
+    for(item of deleteParam){
+        item.addEventListener('click',(e)=>{
+            e.target.parentElement.remove();
+        })
+    }
+
+    addedParamCount++;
+    
+});
+
+// if the user click the submit button 
+let submit = document.getElementById('submit');
